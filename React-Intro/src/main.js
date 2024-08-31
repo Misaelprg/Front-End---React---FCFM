9d83436054f5fabb90ac96814c1c84266fef2624
Misaelprg <mrkenkkenk@gmail.com>
2024-08-30 21:36:32 -0600

    VariablesyConstantes

---
+++ React-Intro/src/main.js
@@ -0,0 +1,13 @@
+let name = "Misael";
+
+name = "Carlos";
+
+const lastname = "Fernandez"
+
+
+if(true) {
+    const lastname = "Lomas";
+    console.log(lastname);
+}
+
+console.log(`Hola ${name} ${lastname}`);
