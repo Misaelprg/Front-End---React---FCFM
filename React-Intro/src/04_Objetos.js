3e6a82c9419803362de04b53306f1be6bbdd2646
Misaelprg <mrkenkkenk@gmail.com>
2024-09-06 20:49:48 -0600

    Objetos En JS

---
+++ React-Intro/src/04_Objetos.js
@@ -0,0 +1,14 @@
+
+const carro = {
+    id: 1,
+    nombre: 'Terzo Millenio', //Uno exotico porque no?
+    date: new Date(),
+    marca: 'Lamborguini',
+    costo: 50000000
+}
+
+carro.nombre = '370z',
+carro.costo = 960000,
+carro.marca = 'Nissan'
+
+console.log(carro.nombre, carro.costo);
